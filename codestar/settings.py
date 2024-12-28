dcd2a16f486e39cfab887b8b6128178c2085883d
kelsey adams <kelseyjaneadams3@gmail.com>
2024-12-28 13:15:45 +0000

    Configure database with environment variables

--- codestar/settings.py
+++ codestar/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-qz*)%*qyj-2&e=r#!+(fh7%t9xpvd%v*snhi8-(wh_mf_nu#pn"
+SECRET_KEY = s.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
